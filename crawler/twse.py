b54304bddf77a87b079d5760a8120939d13d1fb5
Hsu-Huahsing <huahsing412@gmail.com>
2024-10-13 15:35:56 +0800

    1

--- crawler/twse.py
+++ crawler/twse.py
@@ -1,17 +1,20 @@
-from StevenTricks.fileop import pickleload, picklesave
+from StevenTricks.fileop import pickleload, picklesave, runninginfo, logfromfolder
 from StevenTricks.dfi import periodictable
-from log import path_get
+from crawler.log import path_get
 from conf import collection
-from os.path import exists,join
+from os.path import exists, join
 from datetime import datetime, timedelta
 
 import pandas as pd
 
 if __name__ == '__main__':
     # 先把log處理好
+    # 先讀取log檔案本身
+    runninginfo()
+    print("LOG處理程序開始")
     dbpath_dic = path_get(db_name="stock_twse_db")
     log_exist = exists(dbpath_dic["source_log"])
-
+    # 簡單判斷log是否存在
     if log_exist is True:
         print("LOG不存在，即將生成新LOG")
         source_log = pickleload(dbpath_dic["source_log"])
@@ -25,7 +28,17 @@ if __name__ == '__main__':
         print("LOG存在，讀取LOG")
         source_log = periodictable(collection)
     print("LOG讀取成功")
+    print("開始更新LOG")
+    # 開始更新log檔
+    source_log = logfromfolder(dbpath_dic["source_dir"], fileinclude=['.pkl'], fileexclude=['log'], direxclude=['stocklist'], dirinclude=[], log=source_log, fillval='succeed')
+    # 比對資料夾內的資料，依照現有存在的資料去比對比較準確，有可能上次抓完，中間有動到資料
+    print("Log檔案更新結束/nLog程序處理結束")
     # log處理結束
+
+    print("開始進行網路資料下載")
+
+
+
 
 
 
