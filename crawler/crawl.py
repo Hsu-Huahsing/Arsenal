98a2c18ccc1ef1b9c215b483d45a5def54fb6941
Hsu-Huahsing <huahsing412@gmail.com>
2025-05-03 22:12:25 +0800

    1

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -6,13 +6,14 @@ Created on Fri May 22 23:22:32 2020
 @author: mac
 """
 
-from StevenTricks.dfi import findval
+from StevenTricks.dfi import findval, periodictable
 from StevenTricks.netGEN import randomheader
-from StevenTricks.file_utils import logfromfolder, picklesave, pickleload
+from StevenTricks.file_utils import logfromfolder, logmaker, picklesave, pickleload, sweep_path, PathWalk_df
 from StevenTricks.process import sleepteller
 from conf import collection, dailycollection, path_dic
-from schema_utils import warehouseinit
-from os import path, remove, makedirs
+from crawler.schema_utils import warehouseinit
+from os import remove, makedirs
+from os.path import join, exists
 from traceback import format_exc
 
 import sys
@@ -21,21 +22,41 @@ import pandas as pd
 import datetime
 
 dbpath = path_dic["stock_twse_db"]
-
+dbpath_source = join(dbpath, "source")
+dbpath_log = join(dbpath_source, "log", "log.pkl")
+dbpath_errorlog = join(dbpath_source, "log", "errorlog.pkl")
+log_info = sweep_path(dbpath_log)
+errorlog_info = sweep_path(dbpath_errorlog)
 if __name__ == "__main__":
     warehouseinit(dbpath)
     # 每一次被當成主要模組呼叫，都會自動生成倉庫資料夾，為了確保一定有資料夾，所以每次使用都要呼叫一次
-
-
-    log = stocklog.findlog('source', 'log.pkl', collection)
-    # 先找到source(來源資料夾)，裡面有找log.pkl記錄檔，就知道之前抓到哪裡，可以延續抓下去，如果沒有給定週期表(periodictable)，就會回傳空的dataframe
-
-    errorlog = stocklog.findlog('source', 'errorlog.pkl')
-    # 要先參考之前的error log，除了延續之前的資料，還可以修正error的部分
-    # errorlog如果是空的，可以直接創一個空的df
-
-    log = logfromfolder(path.join(db_path, 'source'), fileinclude=['.pkl'], fileexclude=['log'], direxclude=['stocklist'], dirinclude=[], log=log, fillval='succeed')
+    # 先判斷有沒有log
+    if log_info["exists"] is True:
+        log = pickleload(dbpath_log)
+        # 有log還要判斷是不是最新的
+        if datetime.date.today() not in log.index:
+            print("{} not in log index, updating the log table".format(str(datetime.date.today())))
+            latest_log = periodictable(collection, datemin=log.index.max()+datetime.timedelta(days=1))
+            # 從上一次創建log的最新天數開始，所以要加一天，然後開始創建新的table
+            log = pd.concat([log, latest_log])
+    else:
+        log = periodictable(collection)
+    # 不管有沒有log，在爬蟲啟動之前都會根據目前資料夾的資料來更新log，確保抓取沒有遺漏
+    # 再判斷有沒有errorlog
+    if errorlog_info["exists"] is True:
+        errorlog = pickleload(dbpath_errorlog)
+    else:
+        errorlog = pd.DataFrame()
+    print("LOG讀取成功")
+    print("開始更新LOG")
+    # 先盤點資料
+    dbpath_lis = PathWalk_df(dbpath_source, [], ["log"], [], [])
+    # 再更新log檔
+    log = logfromfolder(dbpath_lis, log=log, fillval='succeed')
     # 比對資料夾內的資料，依照現有存在的資料去比對比較準確，有可能上次抓完，中間有動到資料
+    print("Log檔案更新結束\nLog程序處理結束")
+    # log處理結束
+    print("開始進行網路資料下載")
 
     for _ in dailycollection['stocklist']['modelis']:
         df = pd.read_html(dailycollection['stocklist']['url'].format(str(_)), encoding='cp950')
@@ -67,8 +88,7 @@ if __name__ == "__main__":
         else:
             # 沒有產品名，就單獨一個dataframe，那就不用特別處理直接儲存就好
             table = "無細項分類的商品{}".format(str(_))
-            datapath = path.join(db_path, 'source', 'stocklist', table, datetime.datetime.today().strftime(table+'_%Y-%m-%d.pkl'))
-
+            datapath = join(dbpath_source, 'stocklist', table, datetime.datetime.today().strftime(table+'_%Y-%m-%d.pkl'))
             picklesave(df, datapath)
             continue
         # 利用同一個row的重複值來判斷商品項目名稱，同時判斷儲存的方式
@@ -86,14 +106,14 @@ if __name__ == "__main__":
             else:
                 df_sub = df[startint + 1:endint]
 
-            datapath = path.join(db_path, 'source', 'stocklist', startname, datetime.datetime.today().strftime(startname + str(_) + '_%Y-%m-%d.pkl'))
+            datapath = join(dbpath_source, 'stocklist', startname, datetime.datetime.today().strftime(startname + str(_) + '_%Y-%m-%d.pkl'))
             picklesave(df_sub, datapath)
 
     # for ind, col in findval(log.drop(['每日收盤行情', '信用交易統計', '市場成交資訊', '三大法人買賣金額統計表', '三大法人買賣超日報', '個股日本益比、殖利率及股價淨值比', '信用額度總量管制餘額表', '當日沖銷交易標的及成交量值', "每月當日沖銷交易標的及統計", '外資及陸資投資持股統計'], axis=1), 'wait'):
     for ind, col in findval(log, 'wait'):
         crawlerdic = collection[col]
         crawlerdic['payload']['date'] = ind.date().strftime("%Y%m%d")
-        datapath = path.join(db_path, 'source', col)
+        datapath = join(dbpath_source, col)
         print(ind, col)
         makedirs(datapath, exist_ok=True)
 
@@ -105,8 +125,8 @@ if __name__ == "__main__":
             sleepteller()
         except KeyboardInterrupt:
             print("KeyboardInterrupt ... content saving")
-            stocklog.savelog(log, logtype='source', kind='log.pkl')
-            stocklog.savelog(log, logtype='source', kind='errorlog.pkl')
+            picklesave(log,dbpath_log)
+            picklesave(log,dbpath_errorlog)
             print("Log saved .")
             sys.exit()
         except Exception as e:
@@ -123,8 +143,8 @@ if __name__ == "__main__":
                         'errormessage2': e,
                         'errormessage3': 'request failed'}
             errorlog.loc[ind, col] = [errordic]
-            stocklog.savelog(log, logtype='source', kind='log.pkl')
-            stocklog.savelog(log, logtype='source', kind='errorlog.pkl')
+            picklesave(log, dbpath_log)
+            picklesave(log, dbpath_errorlog)
             sleepteller(mode='long')
             continue
 
@@ -137,7 +157,7 @@ if __name__ == "__main__":
             else:
                 # 例假日或颱風假
                 log.loc[log.index == ind, col] = 'close'
-                stocklog.savelog(log, logtype='source', kind='log.pkl')
+                picklesave(log, dbpath_log)
                 continue
         else:
             print("Unknowned error")
@@ -148,20 +168,20 @@ if __name__ == "__main__":
                         'requeststatus': res.status_code,
                         'errormessage1': 'result error'}
             errorlog.loc[ind, col] = [errordic]
-            stocklog.savelog(log, logtype='source', kind='log.pkl')
-            stocklog.savelog(log, logtype='source', kind='errorlog.pkl')
+            picklesave(log, dbpath_log)
+            picklesave(log, dbpath_errorlog)
             sleepteller(mode='long')
             # continue
             exit(0)
         data['crawlerdic'] = crawlerdic
         data['request'] = res
-        picklesave(data=data, path=path.join(datapath, col+'_'+str(ind.date()))+'.pkl')
+        picklesave(data=data, path=join(datapath, col+'_'+str(ind.date()))+'.pkl')
 
         # 把以月為頻率的資料要刪除之前的資料，留當月最新的就好，不用每天都留
         if crawlerdic['freq'] == 'M':
             daterange = pd.date_range(start=ind.strftime('%Y-%m-1'), end=ind-datetime.timedelta(days=1), freq='D', inclusive='left')
             for d in daterange:
-                if path.exists(path.join(datapath, col+'_'+str(d))+'.pkl'):
-                    remove(path.join(datapath, col+'_'+str(d))+'.pkl')
+                if exists(join(datapath, col+'_'+str(d))+'.pkl'):
+                    remove(join(datapath, col+'_'+str(d))+'.pkl')
 
-        stocklog.savelog(log, logtype='source', kind='log.pkl')
+        picklesave(log, dbpath_log)
