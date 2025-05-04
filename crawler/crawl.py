9c637e482d219c953979f7e27df404866dd6c5b0
Hsu-Huahsing <huahsing412@gmail.com>
2025-05-04 15:34:59 +0800

    1

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -6,13 +6,14 @@ Created on Fri May 22 23:22:32 2020
 @author: mac
 """
 
-from StevenTricks.dfi import findval, periodictable
+from StevenTricks.dfi import findval, periodictable, DataFrameMerger
 from StevenTricks.netGEN import randomheader
 from StevenTricks.file_utils import logfromfolder, logmaker, picklesave, pickleload, sweep_path, PathWalk_df
 from StevenTricks.process import sleepteller
 from conf import collection, dailycollection, path_dic, product_col, product_clean
 from crawler.schema_utils import warehouseinit
 from crawler.product_list import product_list
+from crawler.run_crawler_task import main
 from os import remove, makedirs
 from os.path import join, exists
 from traceback import format_exc
@@ -26,8 +27,11 @@ dbpath = path_dic["stock_twse_db"]
 dbpath_source = join(dbpath, "source")
 dbpath_log = join(dbpath_source, "log", "log.pkl")
 dbpath_errorlog = join(dbpath_source, "log", "errorlog.pkl")
+dbpath_productlist = join(dbpath_source, "productlist.pkl")
 log_info = sweep_path(dbpath_log)
 errorlog_info = sweep_path(dbpath_errorlog)
+productlist_info = sweep_path(dbpath_productlist)
+
 if __name__ == "__main__":
     warehouseinit(dbpath)
     # 每一次被當成主要模組呼叫，都會自動生成倉庫資料夾，為了確保一定有資料夾，所以每次使用都要呼叫一次
@@ -58,11 +62,6 @@ if __name__ == "__main__":
     print("Log檔案更新結束\nLog程序處理結束")
     # log處理結束
     print("開始進行網路資料下載")
-
-
-
-
-
     product_dic = {}
     # 先進行商品清單下載
     for _ in dailycollection['stocklist']['modelis']:
@@ -72,87 +71,27 @@ if __name__ == "__main__":
         sleepteller()
     # 針對商品清單做資料清理
     for key in product_dic:
-        break
         product_df = product_dic[key]
-
         product_df = product_df.rename(columns=product_col)
         product_df = product_df.replace({"\u3000": ""}, regex=True)
-        product_df["代號"] = product_df["國際證券辨識號碼(ISIN Code)"].str.slice(product_col[key][0],product_col[key][1])
+        if "國際證券辨識號碼(ISIN Code)" in product_df:
+            product_df["代號"] = product_df["國際證券辨識號碼(ISIN Code)"].str.slice(product_clean["code"][key][0],product_clean["code"][key][1])
         product_dic[key] = product_df
 
-    # for ind, col in findval(log.drop(['每日收盤行情', '信用交易統計', '市場成交資訊', '三大法人買賣金額統計表', '三大法人買賣超日報', '個股日本益比、殖利率及股價淨值比', '信用額度總量管制餘額表', '當日沖銷交易標的及成交量值', "每月當日沖銷交易標的及統計", '外資及陸資投資持股統計'], axis=1), 'wait'):
-    for ind, col in findval(log, 'wait'):
-        crawlerdic = collection[col]
-        crawlerdic['payload']['date'] = ind.date().strftime("%Y%m%d")
-        datapath = join(dbpath_source, col)
-        print(ind, col)
-        makedirs(datapath, exist_ok=True)
+    product = pd.concat(list(product_dic.values()),axis=0)
+    if productlist_info["exists"] is False:
+        picklesave(product, dbpath_productlist)
+    else:
+        product_old = pickleload(dbpath_productlist)
+        product_manager = DataFrameMerger(product_old)
+        product_old = product_manager.renew(product)
 
-        try:
-            res = re.post(url=crawlerdic['url'], headers=next(randomheader()), data=crawlerdic['payload'], timeout=(3, 7))
-            # 有時候會出現回應ok，回應是200的狀況，但是json()會出現error，這樣也是當作錯誤，這是因為網路被反爬蟲的關係
-            data = res.json()
-            print('sleep ...')
-            sleepteller()
-        except KeyboardInterrupt:
-            print("KeyboardInterrupt ... content saving")
-            picklesave(log,dbpath_log)
-            picklesave(log,dbpath_errorlog)
-            print("Log saved .")
-            sys.exit()
-        except Exception as e:
-            print("Unknowned error")
-            print("===============")
-            print(format_exc())
-            # 較詳細的錯誤訊息
-            print("===============")
-            print(e)
-            # 較簡陋的錯誤訊息
-            log.loc[log.index == ind, col] = 'request error'
-            errordic = {'crawlerdic': crawlerdic,
-                        'errormessage1': format_exc(),
-                        'errormessage2': e,
-                        'errormessage3': 'request failed'}
-            errorlog.loc[ind, col] = [errordic]
-            picklesave(log, dbpath_log)
-            picklesave(log, dbpath_errorlog)
-            sleepteller(mode='long')
-            continue
+    main(
+        collection=collection,
+        log=log,
+        errorlog=errorlog,
+        dbpath_source=dbpath_source,
+        dbpath_log=dbpath_log,
+        dbpath_errorlog=dbpath_errorlog
+    )
 
-        if res.status_code == re.codes.ok:
-            # 只要result的結果是正確，且json()又不出錯，大概就一定有正確資料，就只剩下是有資料還是當天休市的差別
-            print(data['stat'])
-            print('------------------')
-            if data['stat'] == 'OK':
-                log.loc[log.index == ind, col] = 'succeed'
-            else:
-                # 例假日或颱風假
-                log.loc[log.index == ind, col] = 'close'
-                picklesave(log, dbpath_log)
-                continue
-        else:
-            print("Unknowned error")
-            print("===============")
-            log.loc[log.index == ind, col] = 'result error'
-            errordic = {'crawlerdic': crawlerdic,
-                        'request': res,
-                        'requeststatus': res.status_code,
-                        'errormessage1': 'result error'}
-            errorlog.loc[ind, col] = [errordic]
-            picklesave(log, dbpath_log)
-            picklesave(log, dbpath_errorlog)
-            sleepteller(mode='long')
-            # continue
-            exit(0)
-        data['crawlerdic'] = crawlerdic
-        data['request'] = res
-        picklesave(data=data, path=join(datapath, col+'_'+str(ind.date()))+'.pkl')
-
-        # 把以月為頻率的資料要刪除之前的資料，留當月最新的就好，不用每天都留
-        if crawlerdic['freq'] == 'M':
-            daterange = pd.date_range(start=ind.strftime('%Y-%m-1'), end=ind-datetime.timedelta(days=1), freq='D', inclusive='left')
-            for d in daterange:
-                if exists(join(datapath, col+'_'+str(d))+'.pkl'):
-                    remove(join(datapath, col+'_'+str(d))+'.pkl')
-
-        picklesave(log, dbpath_log)
---
+++ crawler/run_crawler_task.py
@@ -0,0 +1,179 @@
+# === 基礎套件與自定義工具 ===
+import sys
+import datetime
+import pandas as pd
+from os import makedirs, remove
+from os.path import join, exists
+from traceback import format_exc
+import requests
+
+# 匯入你自己的工具套件（StevenTricks）
+from StevenTricks.dfi import findval                 # 找出 log 中需要處理的資料
+from StevenTricks.netGEN import randomheader         # 隨機 HTTP 請求 header（模擬瀏覽器）
+from StevenTricks.file_utils import picklesave       # 儲存 .pkl 檔案工具
+from StevenTricks.process import sleepteller         # 控制 sleep 時間，避免觸發反爬蟲
+
+# === 配置類別 ===
+class CrawlerConfig:
+    """
+    封裝爬蟲任務需要的所有變數與資源。
+    """
+    def __init__(self, collection, log, errorlog,
+                 dbpath_source, dbpath_log, dbpath_errorlog):
+        self.collection = collection                    # 爬蟲任務配置（每個 col 對應的設定）
+        self.log = log                                  # 任務執行狀態記錄
+        self.errorlog = errorlog                        # 詳細錯誤紀錄
+        self.dbpath_source = dbpath_source              # 資料儲存路徑
+        self.dbpath_log = dbpath_log                    # log 儲存路徑
+        self.dbpath_errorlog = dbpath_errorlog          # error log 儲存路徑
+        self.request_module = requests                  # HTTP 請求模組
+        self.randomheader = randomheader                # 隨機 header 函式
+        self.picklesave = picklesave                    # 儲存函式
+        self.sleepteller = sleepteller                  # sleep 控制函式
+
+# === 執行任務主體 ===
+class CrawlerTask:
+    """
+    執行單一筆爬蟲任務，並處理資料儲存、錯誤處理與 log 記錄。
+    """
+    def __init__(self, config: CrawlerConfig):
+        self.cfg = config
+
+    def run_task(self, ind, col):
+        # 依照日期與欄位名稱取得爬蟲設定
+        crawlerdic = self.cfg.collection[col]
+        crawlerdic['payload']['date'] = ind.date().strftime("%Y%m%d")
+
+        # 建立儲存目錄
+        datapath = join(self.cfg.dbpath_source, col)
+        makedirs(datapath, exist_ok=True)
+        print(ind, col)
+
+        try:
+            # 發送 POST 請求
+            res = self.cfg.request_module.post(
+                url=crawlerdic['url'],
+                headers=next(self.cfg.randomheader()),
+                data=crawlerdic['payload'],
+                timeout=(3, 7)
+            )
+            # 嘗試解析 JSON（可能會因反爬被攔截）
+            data = res.json()
+            print('sleep ...')
+            self.cfg.sleepteller()
+
+        except KeyboardInterrupt:
+            # 人為中斷時儲存資料後退出
+            self._handle_interrupt()
+
+        except Exception as e:
+            # 任意錯誤（連線失敗、json 解析錯、反爬頁面等）
+            self._handle_request_error(ind, col, crawlerdic, e)
+            return
+
+        # 伺服器回應正常（200 OK）
+        if res.status_code == self.cfg.request_module.codes.ok:
+            if data.get('stat') == 'OK':
+                self.cfg.log.loc[self.cfg.log.index == ind, col] = 'succeed'
+            else:
+                self.cfg.log.loc[self.cfg.log.index == ind, col] = 'close'
+                self.cfg.picklesave(self.cfg.log, self.cfg.dbpath_log)
+                return
+        else:
+            # 非 200 回應（如 403、500），需記錄錯誤
+            self._handle_status_error(ind, col, crawlerdic, res)
+            sys.exit(0)
+
+        # 組裝資料儲存
+        data['crawlerdic'] = crawlerdic
+        data['request'] = res
+        filename = join(datapath, f"{col}_{ind.date()}.pkl")
+        self.cfg.picklesave(data, filename)
+
+        # 若是月頻，清除當月舊檔案
+        if crawlerdic.get('freq') == 'M':
+            self._clean_monthly_files(ind, col, datapath)
+
+        # 儲存更新後 log
+        self.cfg.picklesave(self.cfg.log, self.cfg.dbpath_log)
+
+    # === 中斷處理 ===
+    def _handle_interrupt(self):
+        print("KeyboardInterrupt ... content saving")
+        self.cfg.picklesave(self.cfg.log, self.cfg.dbpath_log)
+        self.cfg.picklesave(self.cfg.errorlog, self.cfg.dbpath_errorlog)
+        print("Log saved .")
+        sys.exit()
+
+    # === 請求過程出錯（無回應、json fail） ===
+    def _handle_request_error(self, ind, col, crawlerdic, e):
+        print("Unknowned error")
+        print("===============")
+        print(format_exc())
+        print("===============")
+        print(e)
+        self.cfg.log.loc[self.cfg.log.index == ind, col] = 'request error'
+        errordic = {
+            'crawlerdic': crawlerdic,
+            'errormessage1': format_exc(),
+            'errormessage2': e,
+            'errormessage3': 'request failed'
+        }
+        self.cfg.errorlog.loc[ind, col] = [errordic]
+        self.cfg.picklesave(self.cfg.log, self.cfg.dbpath_log)
+        self.cfg.picklesave(self.cfg.errorlog, self.cfg.dbpath_errorlog)
+        self.cfg.sleepteller(mode='long')
+
+    # === 回應狀態錯誤（如 403, 500） ===
+    def _handle_status_error(self, ind, col, crawlerdic, res):
+        print("Unknowned error")
+        print("===============")
+        self.cfg.log.loc[self.cfg.log.index == ind, col] = 'result error'
+        errordic = {
+            'crawlerdic': crawlerdic,
+            'request': res,
+            'requeststatus': res.status_code,
+            'errormessage1': 'result error'
+        }
+        self.cfg.errorlog.loc[ind, col] = [errordic]
+        self.cfg.picklesave(self.cfg.log, self.cfg.dbpath_log)
+        self.cfg.picklesave(self.cfg.errorlog, self.cfg.dbpath_errorlog)
+        self.cfg.sleepteller(mode='long')
+
+    # === 月頻資料：只留當天，刪除當月過往 ===
+    def _clean_monthly_files(self, ind, col, datapath):
+        daterange = pd.date_range(
+            start=ind.strftime('%Y-%m-1'),
+            end=ind - datetime.timedelta(days=1),
+            freq='D',
+            inclusive='left'
+        )
+        for d in daterange:
+            oldfile = join(datapath, f"{col}_{d}.pkl")
+            if exists(oldfile):
+                remove(oldfile)
+
+# === 主控制邏輯 ===
+def main(collection, log, errorlog, dbpath_source, dbpath_log, dbpath_errorlog):
+    """
+    建立配置與任務執行器，批次執行所有待處理的資料。
+    """
+    config = CrawlerConfig(
+        collection=collection,
+        log=log,
+        errorlog=errorlog,
+        dbpath_source=dbpath_source,
+        dbpath_log=dbpath_log,
+        dbpath_errorlog=dbpath_errorlog
+    )
+    crawler = CrawlerTask(config)
+
+    for ind, col in findval(log, 'wait'):
+        crawler.run_task(ind, col)
+
+# === 可執行介面 ===
+if __name__ == '__main__':
+    # 這裡可以匯入資料後執行 main(...)
+    # 範例：
+    # main(collection, log, errorlog, 'data/', 'data/log.pkl', 'data/errorlog.pkl')
+    pass
