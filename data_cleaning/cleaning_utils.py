2b7c2ec04f5a1fd72cd0277e86e980e8945e1843
Hsu-Huahsing <huahsing412@gmail.com>
2025-05-26 23:33:15 +0800

    1

--- data_cleaning/cleaning_utils.py
+++ data_cleaning/cleaning_utils.py
@@ -7,7 +7,7 @@ from StevenTricks.dictur import keyinstr
 
 def dict_extract(dict_in, title="title", fields="fields", data="data", group="groups", date=None):
     out_dict = {}
-    out_dict["subitem"] = keyinstr(dict_in[title], colname_dic, dict_in["item"])
+    out_dict["subitem"] = keyinstr(dict_in[title], colname_dic, dict_in[title])
     out_dict["fields"] = dict_in[fields]
     out_dict["data"] = dict_in[data]
     out_dict["date"] = pd.to_datetime(date)
