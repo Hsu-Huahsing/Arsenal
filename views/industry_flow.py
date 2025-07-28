2abfcc22d6554c0363db5a8d86ddaf60cf3597a1
Hsu-Huahsing <huahsing412@gmail.com>
2025-07-28 13:48:29 +0800

    1

--- views/industry_flow.py
+++ views/industry_flow.py
@@ -4,8 +4,10 @@ from StevenTricks.track_utils import cache_name
 
 # 🔹 要建立快取記錄的資料項目（會對每一個建立唯一 code）
 data_list = ["個別產業成交比重", "產業及三大法人", "產業及融資融券"]
-
+data_dict = dict(zip(enumerate(data_list), data_list))
+data = data_dict[0]
 
 # 🔹 若直接執行此模組，將執行更新流程並印出結果
 if __name__ == "__main__":
+    if
     cache_data = cache_name(data_list, cachepath, cachepath_namelog, 5, "fuzzy")
