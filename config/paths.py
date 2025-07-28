5d967b8d140bbc734f483fedf0361440d708fd40
Hsu-Huahsing <huahsing412@gmail.com>
2025-07-28 21:09:35 +0800

    1

--- config/paths.py
+++ config/paths.py
@@ -1,21 +1,34 @@
 from os.path import join
+from config.conf import collection
+from StevenTricks.file_utils import PathWalk_df
 
-
-#因為爬蟲的結果會有很多db要儲存，要先指定warehouse(倉庫)的路徑
+# 指定warehouse(倉庫)的路徑
 path_dic = {
     "stock_twse_db": {
-        "db":r"/Users/stevenhsu/Library/Mobile Documents/com~apple~CloudDocs/warehouse/stock/twse",
-        "cache":r"/Users/stevenhsu/Documents/stock/twse"
+        "db": r"/Users/stevenhsu/Library/Mobile Documents/com~apple~CloudDocs/warehouse/stock/twse",
+        "cache": r"/Users/stevenhsu/Documents/stock/twse"
     }
 }
 
 dbpath = path_dic["stock_twse_db"]["db"]
 dbpath_source = join(dbpath, "source")
 dbpath_cleaned = join(dbpath, "cleaned")
-dbpath_cleaned_log = join(dbpath_cleaned,"log.pkl")
+dbpath_cleaned_log = join(dbpath_cleaned, "log.pkl")
 dbpath_log = join(dbpath_source, "log", "log.pkl")
 dbpath_errorlog = join(dbpath_source, "log", "errorlog.pkl")
 dbpath_productlist = join(dbpath_source, "productlist.pkl")
 
 cachepath = path_dic["stock_twse_db"]["cache"]
 cachepath_namelog = join(cachepath, "namelog.pkl")
+
+dbpath_cleaned_db_df = PathWalk_df(dbpath_cleaned, fileinclude=[".db"])
+
+cleaned_db_dict = {}
+
+for key in collection:
+    paths = dbpath_cleaned_db_df.loc[dbpath_cleaned_db_df["file"] == f"{key}.db", "path"].tolist()
+
+    if len(paths) == 1:
+        cleaned_db_dict[key] = paths[0]
+    else:
+        raise ValueError(f"Error: Expected exactly 1 path for '{key}.db', but found {len(paths)} paths: {paths}")
--- views/industry_flow.py
+++ views/industry_flow.py
@@ -1,13 +1,14 @@
 # 導入自定義的快取與名稱紀錄檔路徑
-from config.paths import cachepath, cachepath_namelog
+from config.paths import cachepath, cachepath_namelog, cleaned_db_dict
 from StevenTricks.track_utils import cache_name
-
+from StevenTricks.dbsqlite import readsql_iter
 # 🔹 要建立快取記錄的資料項目（會對每一個建立唯一 code）
 data_list = ["個別產業成交比重", "產業及三大法人", "產業及融資融券"]
-data_dict = dict(zip(enumerate(data_list), data_list))
-data = data_dict[0]
+data_dict = {i: v for i, v in enumerate(data_list)}
+data_name = data_dict[0]
 
 # 🔹 若直接執行此模組，將執行更新流程並印出結果
 if __name__ == "__main__":
-    if
-    cache_data = cache_name(data_list, cachepath, cachepath_namelog, 5, "fuzzy")
+    if data_name == "個別產業成交比重":
+        base_df = readsql_iter()
+        cache_data = cache_name(data_list, cachepath, cachepath_namelog, 5, "fuzzy")
