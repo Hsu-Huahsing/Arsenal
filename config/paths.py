e862c8a5f182b15e95f0fe87e97ac13a9a519ae7
Hsu-Huahsing <huahsing412@gmail.com>
2025-08-05 07:31:54 +0800

    1

--- config/paths.py
+++ config/paths.py
@@ -1,6 +1,7 @@
 from os.path import join
 from config.conf import collection
 from StevenTricks.file_utils import PathWalk_df
+from pathlib import Path
 
 # 指定warehouse(倉庫)的路徑
 path_dic = {
@@ -21,14 +22,16 @@ dbpath_productlist = join(dbpath_source, "productlist.pkl")
 cachepath = path_dic["stock_twse_db"]["cache"]
 cachepath_namelog = join(cachepath, "namelog.pkl")
 
-dbpath_cleaned_db_df = PathWalk_df(dbpath_cleaned, fileinclude=[".pkl"],level=2)
+dbpath_cleaned_db_df = PathWalk_df(dbpath_cleaned, fileinclude=[".pkl"], level=2)
 
 cleaned_db_dict = {}
 
 for key in collection:
-    paths = dbpath_cleaned_db_df.loc[dbpath_cleaned_db_df["file"] == f"{key}.db", "path"].tolist()
+    # 改為根據父資料夾名稱（即資料夾名稱等於 key）
+    paths = dbpath_cleaned_db_df.loc[dbpath_cleaned_db_df["dir"] == key, "path"].tolist()
 
-    if len(paths) == 1:
-        cleaned_db_dict[key] = paths[0]
+    if len(paths) >= 1:
+        # 如果資料夾下有多個 .pkl 檔案，只要挑其中一個檔案的路徑，取其上層資料夾即可
+        cleaned_db_dict[key] = str(Path(paths[0]).parent)
     else:
-        raise ValueError(f"Error: Expected exactly 1 path for '{key}.db', but found {len(paths)} paths: {paths}")
+        raise ValueError(f"Error: Expected at least 1 file under folder '{key}', but found 0.")
