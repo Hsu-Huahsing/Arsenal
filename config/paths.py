3e8b2bdd07154f3dfa42eab48421f0a92a24ecfa
Hsu-Huahsing <huahsing412@gmail.com>
2025-08-06 07:26:21 +0800

    1

--- config/paths.py
+++ config/paths.py
@@ -1,37 +1,23 @@
-from os.path import join
-from config.conf import collection
-from StevenTricks.file_utils import PathWalk_df
 from pathlib import Path
 
-# 指定warehouse(倉庫)的路徑
+# 指定 warehouse 路徑
 path_dic = {
     "stock_twse_db": {
-        "db": r"/Users/stevenhsu/Library/Mobile Documents/com~apple~CloudDocs/warehouse/stock/twse",
-        "cache": r"/Users/stevenhsu/Documents/stock/twse"
+        "db": Path("/Users/stevenhsu/Library/Mobile Documents/com~apple~CloudDocs/warehouse/stock/twse"),
+        "cache": Path("/Users/stevenhsu/Documents/stock/twse")
     }
 }
 
-dbpath = path_dic["stock_twse_db"]["db"]
-dbpath_source = join(dbpath, "source")
-dbpath_cleaned = join(dbpath, "cleaned")
-dbpath_cleaned_log = join(dbpath_cleaned, "log.pkl")
-dbpath_log = join(dbpath_source, "log", "log.pkl")
-dbpath_errorlog = join(dbpath_source, "log", "errorlog.pkl")
-dbpath_productlist = join(dbpath_source, "productlist.pkl")
+db_root = path_dic["stock_twse_db"]["db"]
+cache_root = path_dic["stock_twse_db"]["cache"]
 
-cachepath = path_dic["stock_twse_db"]["cache"]
-cachepath_namelog = join(cachepath, "namelog.pkl")
+# 資料路徑整理
+dbpath_source = db_root / "source"
+dbpath_cleaned = db_root / "cleaned"
+dbpath_cleaned_log = dbpath_cleaned / "log.pkl"
+dbpath_log = dbpath_source / "log" / "log.pkl"
+dbpath_errorlog = dbpath_source / "log" / "errorlog.pkl"
+dbpath_productlist = dbpath_source / "productlist.pkl"
 
-dbpath_cleaned_db_df = PathWalk_df(dbpath_cleaned, fileinclude=[".pkl"], level=2)
+cachepath_namelog = cache_root / "namelog.pkl"
 
-cleaned_db_dict = {}
-
-for key in collection:
-    # 改為根據父資料夾名稱（即資料夾名稱等於 key）
-    paths = dbpath_cleaned_db_df.loc[dbpath_cleaned_db_df["dir"] == key, "path"].tolist()
-
-    if len(paths) >= 1:
-        # 如果資料夾下有多個 .pkl 檔案，只要挑其中一個檔案的路徑，取其上層資料夾即可
-        cleaned_db_dict[key] = str(Path(paths[0]).parent)
-    else:
-        raise ValueError(f"Error: Expected at least 1 file under folder '{key}', but found 0.")
